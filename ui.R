bd315eeb248f4a4f4b8181afb17a542b1eef8bfa
Elaina Bellone <ebellone@mail.wlu.edu>
2026-03-02 15:59:40 -0500

    cant figure out parentheses

--- ui.R
+++ ui.R
@@ -2,28 +2,32 @@ library(shinydashboard)
 library(shiny)
 
 dashboardPage(
-  dashboardHeader(Title = "Groceries in the US"),
+  dashboardHeader(title = "Groceries in the US"),
   dashboardSidebar(
     sidebarMenu(
       menuItem("Dollars spent by state per category", tabName = "BarByState")
     )
   ),
   dashboardBody(
-    fluidPage(
-      titlePanel("outputstates.csv"),
-      sidebarLayout(
-        sidebarPanel(
-          selectInput(
-            inputId  = "state",
-            label    = "Select a State:",
-            choices  = sort(unique(outputstates$State)),
-            selected = "California"
-          )
-        ),
-        mainPanel(
-          plotOutput("BarByState")
-        )
-      )
+    tabItems(
+      tabItem(tabName = "Dollars spent by state per category",
+              fluidPage(
+                titlePanel("outputstates.csv"),
+                sidebarLayout(
+                  sidebarPanel(
+                    selectInput(
+                      inputId  = "state",
+                      label    = "Select a State:",
+                      choices  = sort(unique(outputstates$State)),
+                      selected = "California"
+                    )
+                  ),
+                  mainPanel(
+                    plotOutput("BarByState")
+                  )
+                )
+              )    
+              )
     )
   )
 )
