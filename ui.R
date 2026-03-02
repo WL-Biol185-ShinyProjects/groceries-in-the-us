b2c1a25542fc8db4b4dbdaf02d3f1b0dd0f271b6
Elaina Bellone <ebellone@mail.wlu.edu>
2026-03-02 15:52:39 -0500

    workshopping shiny app

--- ui.R
+++ ui.R
@@ -1,21 +1,29 @@
+library(shinydashboard)
 library(shiny)
 
-fluidPage(
-  
-  titlePanel("outputstates.csv"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      selectInput(
-        inputId  = "state",
-        label    = "Select a State:",
-        choices  = sort(unique(outputstates$State)),
-        selected = "California"
+dashboardPage(
+  dashboardHeader(Title = "Groceries in the US"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Dollars spent by state per category", tabName = "BarByState")
+    )
+  ),
+  dashboardBody(
+    fluidPage(
+      titlePanel("outputstates.csv"),
+      sidebarLayout(
+        sidebarPanel(
+          selectInput(
+            inputId  = "state",
+            label    = "Select a State:",
+            choices  = sort(unique(outputstates$State)),
+            selected = "California"
+          )
+        ),
+        mainPanel(
+          plotOutput("BarByState")
+        )
       )
-    ),
-    
-    mainPanel(
-      plotOutput("BarByState")
     )
   )
 )
