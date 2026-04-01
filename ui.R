72ec72c61807cf46bc3be5350fba88da2352cd48
Shriner <Nate Shriner>
2026-04-01 16:08:16 -0400

    fixing spelling mistake

--- ui.R
+++ ui.R
@@ -656,7 +656,7 @@ dashboardPage(
                            h4("Nate Shriner", style = "color: #e76f51; font-weight: 600; margin-bottom: 8px;"),
                            p(style = "font-size: 13px; color: #666;",
                              "Nate Shriner is a junior at Washington & Lee studying Neuroscience with the aspiration of becoming a doctor one day. 
-                             He believes the best medicine is nutrition and fitness. His favorite groccery store is H-E-B, and his favorite groccery category is meats, eggs, and nuts.")
+                             He believes the best medicine is nutrition and fitness. His favorite grocery store is H-E-B, and his favorite groccery category is meats, eggs, and nuts.")
                          )
                   ),
                   column(4,
